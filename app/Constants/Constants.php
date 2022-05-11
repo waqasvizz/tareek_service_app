6cf73a723d072648635ac5bff7041a7a523bbbfb
Danish Hussain <danishhussain9525@hotmail.com>
2022-05-11 14:58:00 +0500

    server key update

--- app/Constants/Constants.php
+++ app/Constants/Constants.php
@@ -3,6 +3,6 @@
 namespace App\Constants;
 
     class Constants {
-        const FIREBASE_SERVER_API_KEY = 'AAAAvjorVsA:APA91bHm5UZfc9EbhWOX8aOA17BB8-BYwWlCILRNX-VH3JOSJQU21d7z5dARNcIeT3yujbDuDtk97DdsBwsFACB2ZhyAaEpYhDq3YIq-yFeX2x_b9rDn0x4MSV5kmOr_6JG3S7CifCy5';
+        const FIREBASE_SERVER_API_KEY = 'AAAAy_4vrNQ:APA91bGMw9TYLzmA9mJKIybVZiQiH2btsTqQHASzU0idZrUjpj7_306YV0CLhtdwPfT_UPtOSrc1HqJc34GKIsTuGa-ReJx1ZZjaIVHOT7lil_IIaKVn2fV5FioiVwPBrH3HGqVlPi00';
         const PER_PAGE = 10;
     }
